35241e3703602e9a13055ce43fa3f4a41bf130fd
Shunks <00001130754698sp@al.educacao.sp.gov.br>
2024-05-29 10:34:45 -0300

    adicionando arquivos js e css

---
+++ scketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(600, 600);
+    background(color(0, 0, 0));
+  
+  }
+  
+  function draw() {
+    stroke("purple");
+    fill("pink");
+  
+    
+  if(mouseIsPressed){
+      rect(mouseX, mouseY, 20, 35);
+  }
+  }
